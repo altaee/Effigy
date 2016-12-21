c0d57299fb95760facd7f03cfcbd4eee1295969c
komarovb <fisika80@gmail.com>
2016-12-21 23:20:33 +0100

    Register pop up update from Tomasz

--- app/src/main/java/thesis/effigy/com/effigy/RegisterDialogFragment.java
+++ app/src/main/java/thesis/effigy/com/effigy/RegisterDialogFragment.java
@@ -4,18 +4,23 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
-        builder.setView(inflater.inflate(R.layout.dialog_register, null))
+        View dialogView = inflater.inflate(R.layout.dialog_register, null);
+        builder.setView(dialogView)
                 // Add action buttons
                 .setPositiveButton(R.string.register, new DialogInterface.OnClickListener() {
                     @Override
@@ -30,6 +35,35 @@ public class RegisterDialogFragment extends DialogFragment {
                 })
                 // Set title
                 .setTitle(R.string.register);
+
+        final EditText mPassword = (EditText) dialogView.findViewById(R.id.input_password);
+        final EditText mPasswordConfirm = (EditText) dialogView.findViewById(R.id.input_password_confirm);
+
+        mPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b) {
+                    if (!mPassword.getText().toString().isEmpty()) {
+                        mPasswordConfirm.setEnabled(true);
+                    } else {
+                        mPasswordConfirm.setEnabled(false);
+                        mPasswordConfirm.setText("");
+                    }
+                }
+            }
+        });
+
+        mPasswordConfirm.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (mPassword.getText().toString().equals(mPasswordConfirm.getText().toString())){
+
+                } else {
+
+                }
+            }
+        });
+
         return builder.create();
     }
 }
