d90006dd849c422f48bff600ddf2db6e1d2cf456
komarovb <fisika80@gmail.com>
2016-12-22 02:11:28 +0100

    Buttons style changed

--- app/src/main/java/thesis/effigy/com/effigy/Tab2UploadImage.java
+++ app/src/main/java/thesis/effigy/com/effigy/Tab2UploadImage.java
@@ -1,13 +1,8 @@
 package thesis.effigy.com.effigy;
 
-import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -20,14 +15,6 @@ public class Tab2UploadImage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab2_upload_image, container, false);
-
-        Button finishButton = (Button) rootView.findViewById(R.id.finishButton);
-        finishButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getActivity(), FinalPageActivity.class));
-            }
-        });
 
         Button UploadImage = (Button) rootView.findViewById(R.id.uploadButton);
         UploadImage.setOnClickListener(new View.OnClickListener() {
