c1ef4fcef79599f72777c341baab0ae9543f2e26
komarovb <fisika80@gmail.com>
2016-12-22 03:10:04 +0100

    Basic backend structure added

---
+++ app/src/main/java/thesis/effigy/com/effigy/backend/EvaluationResult.java
@@ -0,0 +1,14 @@
+package thesis.effigy.com.effigy.backend;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Borys on 12/22/16.
+ */
+
+public class EvaluationResult extends AsyncTask<Void,Void,Void> {
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
