bbd78ad1f1e2054ea49f82cd32790b82898baecf
komarovb <fisika80@gmail.com>
2016-12-22 03:04:44 +0100

    Upload image possibility

--- app/src/main/java/thesis/effigy/com/effigy/Tab2UploadImage.java
+++ app/src/main/java/thesis/effigy/com/effigy/Tab2UploadImage.java
@@ -1,14 +1,25 @@
 package thesis.effigy.com.effigy;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class Tab2UploadImage extends Fragment {
+
+    private static final int RESULT_LOAD_IMG = 99;
 
 
     @Override
@@ -20,9 +31,50 @@ public class Tab2UploadImage extends Fragment {
         UploadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                // Create intent to Open Image applications like Gallery, Google Photos
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                // Start the Intent
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
             }
         });
         return rootView;
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContext().getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) getView().findViewById(R.id.uploadImage1);
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+
+            } else {
+                Toast.makeText(getActivity(), "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+    }
+
 }
