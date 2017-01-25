bfbec5dc154dbd47ad7d574d59f8ff903ff034a5
komarovb <fisika80@gmail.com>
2017-01-25 22:06:46 +0100

    Unit tests from Tomek are added

---
+++ app/src/test/java/thesis/effigy/com/effigy/UnitTests.java
@@ -0,0 +1,56 @@
+package thesis.effigy.com.effigy;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import thesis.effigy.com.effigy.adapters.SimilarImagesAdapter;
+import thesis.effigy.com.effigy.data.ParentImage;
+import thesis.effigy.com.effigy.data.SimilarImage;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Tomek on 24.01.2017.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class UnitTests {
+
+    @Test
+    public void parentImageTests() {
+        ParentImage parentImage = new ParentImage(965478952,"http://adress.com/parent.png");
+
+        assertThat(parentImage.toString(), is("ParentImage{parentId='965478952', imageUrl='http://adress.com/parent.png'}"));
+        assertThat(parentImage.getImageUrl(), is("http://adress.com/parent.png"));
+        assertEquals(parentImage.getParentId(), 965478952);
+    }
+
+    @Test
+    public void similarImageTests() {
+        SimilarImage similarImage = new SimilarImage(123456789,965478952,"http://adress.com/img.png",1);
+
+        assertThat(similarImage.toString(), is("SimilarImage{imageId='123456789', imageUrl='http://adress.com/img.png', ranking=1}"));
+
+        similarImage.setRanking(2);
+        assertThat(similarImage.toString(), is("SimilarImage{imageId='123456789', imageUrl='http://adress.com/img.png', ranking=2}"));
+        assertThat(similarImage.getImageUrl(), is("http://adress.com/img.png"));
+        assertEquals(similarImage.getRanking(), (Integer)2);
+        assertEquals(similarImage.getImageId(), 123456789);
+    }
+
+    @Test
+    public void similarImagesAdapterTest() {
+        Context mockContext = Mockito.mock(Context.class);
+        TextView mockTextView = Mockito.mock(TextView.class);
+        SimilarImagesAdapter similarImagesAdapter = new SimilarImagesAdapter(mockContext, mockTextView);
+        assertEquals(similarImagesAdapter.getCount(), 0);
+    }
+
+}
